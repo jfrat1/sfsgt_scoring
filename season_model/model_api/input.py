934f41066743667fa97f474220e1dba350b44dea
John Fratello <jfrat1@gmail.com>
2024-12-02 23:09:52 -0800

    Add a few tests for the season model inputs module

--- season_model/model_api/input.py
+++ season_model/model_api/input.py
@@ -19,20 +19,29 @@ class SeasonModelEventPlayerInput(NamedTuple):
     scorecard: scorecard.Scorecard
     player: player.Player
 
+    @property
+    def player_name(self) -> str:
+        return self.player.name
+
 
 class SeasonModelEventInput(NamedTuple):
+    event_name: str
     course: course_database.Course
     tees: str
     event_type: SeasonModelEventType
     players: list[SeasonModelEventPlayerInput]
 
+    def player_names(self) -> list[str]:
+        return [player.player_name for player in self.players]
 
-class SeasonModelEventInputs(NamedTuple):
-    events: list[SeasonModelEventInput]
 
-    # TODO: Test if this even works. I'm not sure.
-    def __iter__(self) -> Iterator[list[SeasonModelEventInput]]:
-        yield self.events
+class SeasonModelEventInputs:
+    def __init__(self, events: list[SeasonModelEventInput]) -> None:
+        self.events = events
+
+    def __iter__(self) -> Iterator[SeasonModelEventInput]:
+        for event in self.events:
+            yield event
 
 
 class SeasonModelInputConsistencyError(Exception):
@@ -47,9 +56,12 @@ class SeasonModelInput:
         self._verify_input_consistency()
 
     def _verify_input_consistency(self) -> None:
+        sorted_player_names = sorted(self._player_names)
+
         for event_data in self._events:
-            if (event_player_names := event_data.player_names()) != self._player_names:
+            sorted_event_player_names = sorted(event_data.player_names())
+            if sorted_event_player_names != sorted_player_names:
                 raise SeasonModelInputConsistencyError(
                     f"Player names in event {event_data.event_name} do not match expectations.\n"
-                    f"Expected: {self._player_names}. \nFound: {event_player_names}."
+                    f"Expected: {self._player_names}. \nFound: {sorted_event_player_names}."
                 )
---
+++ tests/season_model/model_api/test_input.py
@@ -0,0 +1,74 @@
+from unittest import mock
+
+import pytest
+
+from season_model.model_api import input
+
+
+def test_season_model_event_inputs_iter() -> None:
+    """Test that the __iter__ method of SeasonModelEventInputs works correctly."""
+    stub_event_input_1 = mock.MagicMock(spec=input.SeasonModelEventInput, autospec=True)
+    stub_event_input_1.event_name = "Event 1"
+    stub_event_input_2 = mock.MagicMock(spec=input.SeasonModelEventInput, autospec=True)
+    stub_event_input_2.event_name = "Event 2"
+
+    event_inputs = input.SeasonModelEventInputs(events=[stub_event_input_1, stub_event_input_2])
+
+    for idx, event_input in enumerate(event_inputs):
+        assert event_input.event_name == f"Event {idx + 1}"
+
+
+def test_season_model_input_constructor_verify_input_consistency_ok() -> None:
+    player_names = ["Charlie Brown", "Snoopy"]
+
+    stub_event_input_1 = mock.MagicMock(spec=input.SeasonModelEventInput, autospec=True)
+    stub_event_input_1.player_names.return_value = player_names.copy()
+
+    stub_event_input_2 = mock.MagicMock(spec=input.SeasonModelEventInput, autospec=True)
+    stub_event_input_2.player_names.return_value = player_names.copy()
+
+    event_inputs = input.SeasonModelEventInputs(events=[stub_event_input_1, stub_event_input_2])
+
+    input.SeasonModelInput(
+        player_names=player_names,
+        events=event_inputs,
+    )
+
+
+def test_season_model_input_constructor_verify_input_consistency_out_of_order_names_ok() -> None:
+    player_names = ["Charlie Brown", "Snoopy", "Linus"]
+
+    stub_event_input_1 = mock.MagicMock(spec=input.SeasonModelEventInput, autospec=True)
+    stub_event_input_1.player_names.return_value = ["Snoopy", "Linus", "Charlie Brown"]
+
+    stub_event_input_2 = mock.MagicMock(spec=input.SeasonModelEventInput, autospec=True)
+    stub_event_input_2.player_names.return_value = ["Linus", "Charlie Brown", "Snoopy"]
+
+    event_inputs = input.SeasonModelEventInputs(events=[stub_event_input_1, stub_event_input_2])
+
+    input.SeasonModelInput(
+        player_names=player_names,
+        events=event_inputs,
+    )
+
+
+def test_season_model_input_constructor_verify_input_consistency_fails() -> None:
+    player_names = ["Charlie Brown", "Snoopy"]
+
+    stub_event_input_1 = mock.MagicMock(spec=input.SeasonModelEventInput, autospec=True)
+    stub_event_input_1.player_names.return_value = player_names.copy()
+
+    stub_event_input_2 = mock.MagicMock(spec=input.SeasonModelEventInput, autospec=True)
+    stub_event_input_2.player_names.return_value = ["Wrong Player"]
+
+    event_inputs = input.SeasonModelEventInputs(events=[stub_event_input_1, stub_event_input_2])
+
+    with pytest.raises(input.SeasonModelInputConsistencyError):
+        input.SeasonModelInput(
+            player_names=player_names,
+            events=event_inputs,
+        )
+
+
+
+
