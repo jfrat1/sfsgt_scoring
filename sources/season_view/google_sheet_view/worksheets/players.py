b770a7822c161482cd249525b254bf7028d2b0eb
John Fratello <jfrat1@gmail.com>
2025-03-25 20:45:30 -0700

    Refactor the players worksheet reader to extract a new class

--- sources/season_view/google_sheet_view/worksheets/players.py
+++ sources/season_view/google_sheet_view/worksheets/players.py
@@ -4,8 +4,11 @@ from season_common import player
 
 from season_view.api import read_data
 
+HEADER_ROW = 2
+
 PLAYER_COLUMN = "Golfer"
 GENDER_COLUMN = "Gender"
+FINALE_COLUMN = "Finale"
 
 
 class PlayersWorksheetError(Exception):
@@ -22,18 +25,51 @@ class PlayersWorksheet:
         self.events = events
 
     def read(self) -> read_data.SeasonViewReadPlayers:
-        raw_data = self.worksheet_controller.to_df(header_row=2)
-        self._verify_columns_case_insensitive(raw_data)
+        raw_data = self.worksheet_controller.to_df(header_row=HEADER_ROW)
 
-        headers_lower = [col.lower() for col in raw_data.columns]
-        raw_data.columns = pd.Index(headers_lower)
-        raw_data.set_index(keys=PLAYER_COLUMN.lower(), inplace=True)
+        data = PlayersWorksheetData(raw_data=raw_data, events=self.events)
 
-        are_finale_hcps_available = "finale" in headers_lower
-        events = self.events + ["Finale"] if are_finale_hcps_available else self.events
+        return read_data.SeasonViewReadPlayers(
+            players=data.read_players(),
+            are_finale_hcps_available=data.are_finale_handicaps_available(),
+        )
 
+
+class PlayersWorksheetData:
+    def __init__(self, raw_data: pd.DataFrame, events: list[str]) -> None:
+        self._raw_data = raw_data
+        self._events = events
+
+        self._available_columns_lower = self._available_columns_lower_case()
+        self._are_genders_available = GENDER_COLUMN.lower() in self._available_columns_lower
+        self._are_finale_handicaps_available = FINALE_COLUMN.lower() in self._available_columns_lower
+
+        if self._are_finale_handicaps_available:
+            self._events.append(FINALE_COLUMN.lower())
+
+        self._verify_available_columns()
+        self._cleanse_raw_data()
+
+    def _available_columns_lower_case(self) -> list[str]:
+        return [column.lower() for column in self._raw_data.columns]
+
+    def _verify_available_columns(self) -> None:
+        required_columns = self._events + [PLAYER_COLUMN]
+        required_columns_lower = set(item.lower() for item in required_columns)
+
+        missing_columns = required_columns_lower.difference(self._available_columns_lower)
+
+        if len(missing_columns) != 0:
+            raise PlayersWorksheetError(f"Players worksheet is missing required columns: {missing_columns}")
+
+    def _cleanse_raw_data(self) -> None:
+        self._raw_data.columns = pd.Index(self._available_columns_lower)
+        self._raw_data.set_index(keys=PLAYER_COLUMN.lower(), inplace=True)
+
+    def read_players(self) -> list[read_data.SeasonViewReadPlayer]:
         _players: list[read_data.SeasonViewReadPlayer] = []
-        for player_name, player_data in raw_data.iterrows():
+
+        for player_name, player_data in self._raw_data.iterrows():
             _players.append(
                 read_data.SeasonViewReadPlayer(
                     player=player.Player(
@@ -41,21 +77,12 @@ class PlayersWorksheet:
                         gender=player.PlayerGender(player_data[GENDER_COLUMN.lower()]),
                     ),
                     event_handicap_indices=read_data.SeasonViewEventHandicapIndices(
-                        {event: player_data[event.lower()] for event in events}
+                        {event: player_data[event.lower()] for event in self._events}
                     ),
                 )
             )
 
-        return read_data.SeasonViewReadPlayers(
-            players=_players,
-            are_finale_hcps_available=are_finale_hcps_available,
-        )
+        return _players
 
-    def _verify_columns_case_insensitive(self, raw_data: pd.DataFrame) -> None:
-        required_columns = set(item.lower() for item in self.events + [PLAYER_COLUMN, GENDER_COLUMN])
-        available_columns = set(column.lower() for column in raw_data.columns)
-
-        missing_columns = required_columns.difference(available_columns)
-
-        if len(missing_columns) != 0:
-            raise PlayersWorksheetError(f"Players worksheet is missing required columns: {missing_columns}")
+    def are_finale_handicaps_available(self) -> bool:
+        return self._are_finale_handicaps_available
