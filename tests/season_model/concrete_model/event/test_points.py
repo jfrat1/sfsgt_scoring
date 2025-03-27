dd568e36a099e6ebed09f35aafa51b52f4213b1b
John Fratello <jfrat1@gmail.com>
2025-03-26 19:11:04 -0700

    Move points test.

--- tests/season_model/concrete_model/event/test_points.py
+++ tests/season_model/concrete_model/event/test_points.py
@@ -1,5 +1,7 @@
 import pytest
-from sfsgt_scoring.season.event import inputs, points
+
+from season_model.api.input import SeasonModelEventType
+from season_model.concrete_model.event import points
 
 TEST_PLAYER_RANKS_NO_TIES = {
     "Player_1": 6,
@@ -81,53 +83,53 @@ TEST_PLAYER_POINTS_WITH_TIES_STANDARD_EVENT = {
 
 
 def test_points_construct_standard_event() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
     assert points_._points_by_rank == points.STANDARD_EVENT_POINTS_BY_RANK
 
 
 def test_points_construct_major_event() -> None:
-    points_ = points.Points(inputs.EventType.MAJOR)
+    points_ = points.Points(SeasonModelEventType.MAJOR)
     assert points_._points_by_rank == points.MAJOR_EVENT_POINTS_BY_RANK
 
 
 def test_player_points_from_ranks_no_ties_standard_event() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
     assert points_.player_points_from_ranks(TEST_PLAYER_RANKS_NO_TIES) == TEST_PLAYER_POINTS_NO_TIES_STANDARD_EVENT
 
 
 def test_player_points_from_ranks_with_ties_standard_event() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
     assert points_.player_points_from_ranks(TEST_PLAYER_RANKS_WITH_TIES) == TEST_PLAYER_POINTS_WITH_TIES_STANDARD_EVENT
 
 
 def test_player_points_from_ranks_empty_ranks_returns_empty_dict() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     empty_dict: dict[str, int] = {}
     assert points_.player_points_from_ranks(player_ranks=empty_dict) == empty_dict
 
 
 def test_are_player_ranks_empty_true() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     empty_dict: dict[str, int] = {}
     assert points_._are_player_ranks_empty(empty_dict)
 
 
 def test_are_player_rans_empty_false() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     not_empty_dict: dict[str, int] = {"foo": 1}
     assert not points_._are_player_ranks_empty(not_empty_dict)
 
 
 def test_verify_player_ranks_ok() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
     points_._verify_player_ranks(TEST_PLAYER_RANKS_NO_TIES)
 
 
 def test_verify_player_ranks_raises_error_if_rank_not_in_allowed_range() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     player_ranks = TEST_PLAYER_RANKS_NO_TIES.copy()
 
@@ -141,44 +143,44 @@ def test_verify_player_ranks_raises_error_if_rank_not_in_allowed_range() -> None
 
 
 def test_players_at_each_rank_no_ties() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     assert points_._players_at_each_rank(TEST_PLAYER_RANKS_NO_TIES) == TEST_PLAYERS_AT_EACH_RANK_NO_TIES
 
 
 def test_players_at_each_rank_with_ties() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     assert points_._players_at_each_rank(TEST_PLAYER_RANKS_WITH_TIES) == TEST_PLAYERS_AT_EACH_RANK_WITH_TIES
 
 
 def test_player_points_no_ties_standard_event() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     assert points_._player_points(TEST_PLAYERS_AT_EACH_RANK_NO_TIES) == TEST_PLAYER_POINTS_NO_TIES_STANDARD_EVENT
 
 
 def test_player_points_with_ties_standard_event() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     assert points_._player_points(TEST_PLAYERS_AT_EACH_RANK_WITH_TIES) == TEST_PLAYER_POINTS_WITH_TIES_STANDARD_EVENT
 
 
 def test_points_for_players_at_rank_one_player_standard_event() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     assert points_._points_for_players_at_rank(rank=1, num_players=1) == 50.0
 
 
 def test_points_for_players_at_rank_two_players_standard_event() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     # Points for rank 3 and 4 split evenly
     assert points_._points_for_players_at_rank(rank=3, num_players=2) == 33.75
 
 
 def test_points_for_players_at_rank_three_players_standard_event() -> None:
-    points_ = points.Points(inputs.EventType.STANDARD)
+    points_ = points.Points(SeasonModelEventType.STANDARD)
 
     # Points for rank 6, 7, and 8 split evenly
     assert points_._points_for_players_at_rank(rank=6, num_players=3) == 19.5
